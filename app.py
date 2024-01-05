e8139640549efd76bdce04dd22c5baaf81227544
Aashish-kushwaha <kushwahaashu444@gmail.com>
2024-01-05 15:07:48 +0530

    unwanted code removed

--- app.py
+++ app.py
@@ -7,9 +7,26 @@ from bson import ObjectId
 app=Flask(__name__)
 ALLOWED_EXTENSIONS = {'pptx', 'docx', 'xlsx'}
 
-app.config['UPLOAD_FOLDER']='D:/secure_filetransfer/uploads/'
+app.config['UPLOAD_FOLDER']='uploads'
 
-#========Login function==============================
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload_file')
+def upload_file():
+    return render_template('upload.html')
+
+@app.route('/download')
+def download():
+    rows = list(db.Fileupload.find())
+    print("sdfghjkk")
+    return render_template('download.html',rows=rows)
+
+#   user login function
 @app.route('/login',methods=['GET','POST'])
 def user_login():
     username=request.form.get('username')
@@ -22,53 +39,40 @@ def user_login():
         return redirect(url_for('upload_file'))
     else:
         return render_template('index.html')  
+    
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/upload_file')
-def upload_file():
-    return render_template('upload.html')
-
-  
-
+#   client login function
 @app.route('/client-login',methods=['POST'])
 def client_login():
     clientname=request.form.get('clientname')
     cpassword=request.form.get('password')
     
-    # print(clientname)
-    # print(cpassword)
+    print(clientname)
+    print(cpassword)
 
-    # print(db.LoginUser)
+    print(db.LoginUser)
 
     user= db.Client.find_one({'clientname':clientname})
-    # print(user)
-    # print(user['cpassword'])
+    print(user)
+    print(user['cpassword'])
   
     if user and check_password_hash(user['cpassword'],cpassword):
-        return jsonify({'message':'login successful'})
+        print("aa gya")
+        return redirect(url_for('download'))
     else:
-        return jsonify({'message':'login failed'}),401
+        return render_template('index.html')  
 
 
+#     Client register function
 @app.route('/client-register',methods=['POST'])
 def client_register():
     email=request.form.get('email')
     password=request.form.get('password')
     clientname=request.form.get('clientname')
     collection_name = db.Client.name
-    # print("email:",email)
-    # print("password:",password)
-    # print("clientname:",clientname)
-    # print("collection:",collection_name)
-
-    
     existing_user= db.Client.find_one({'clientemail':email})
-    # print(existing_user)
- 
+    if not email or not password or not clientname:
+        return jsonify({'message':'all fields are required'})
     if existing_user:
         return jsonify({'message':'Email already registered'}),400
     else:
@@ -77,18 +81,8 @@ def client_register():
         return jsonify({'message':'User registered Successfully'}),201
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/download')
-def download():
-    rows = list(db.Fileupload.find())
-    # filenames = [row['filename'] for row in rows]
-    # ids = [str(row['_id']) for row in rows]
-    return render_template('download.html',rows=rows)
-   
-
+ 
+ #     download function
 @app.route('/download_file/<_id>',methods=['GET', 'POST'])
 def download_file(_id):
      obj_id=ObjectId(_id)
@@ -100,16 +94,13 @@ def download_file(_id):
         return send_file(file_info['furl'], as_attachment=True)
      else:
         return 'File not found'
-    
-    # rows = list(db.Fileupload.find())
-    # print(rows)
-    # fileurl = [row['furl'] for row in rows]
-    # print(fileurl)
-    # ifs3
-    # return send_file(fileurl,as_attachment=True)
 
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+#    upload function 
+     
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
--- models.py
+++ models.py
@@ -4,16 +4,14 @@ from urllib.parse import quote_plus
 username = 'kushwahaashu444'
 password = 'ashu'
 
-# Escape the username and password using quote_plus
+
 escaped_username = quote_plus(username)
 escaped_password = quote_plus(password)
 
-# Construct the MongoDB connection string
 connection_string = f"mongodb+srv://{escaped_username}:{escaped_password}@cluster0.g247luz.mongodb.net/"
 
 print(connection_string)
 
-# Create a MongoDB client
 conn = pymongo.MongoClient(connection_string)
 
 db=conn.filesharing
